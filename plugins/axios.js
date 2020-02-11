6144ec394a9096c500346dc809e24c66cfb4fbe8
一打砖头拍不倒 <lyq15360562781@163.com>
2020-02-11 17:48:56 +0800

    机票首页静态渲染完成

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,7 @@
 import {Message} from 'element-ui';
 export default (data)=>{
+    
     data.$axios.onError((err)=>{
-        console.log(err.response);
-        
         const {statusCode, message} = err.response.data;
         if(statusCode === 400){
             Message.warning({message});
