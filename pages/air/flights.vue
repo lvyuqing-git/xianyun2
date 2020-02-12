0827a663dc3f07e8aa4745f0d0a5996eadd8cf5d
一打砖头拍不倒 <lyq15360562781@163.com>
2020-02-12 20:44:47 +0800

    机票信息展示功能完成

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -14,7 +14,17 @@
         <FlightsListHead></FlightsListHead>
 
         <!-- 航班信息 -->
-        <FlightsItem v-for="(item,index) in airTicketList.flights" :key="index" :data="item"></FlightsItem>
+        <FlightsItem v-for="(item,index) in airTicketList.flights"
+                     :key="index"
+                     :data="item"></FlightsItem>
+        <el-pagination @size-change="handleSizeChange"
+                       @current-change="handleCurrentChange"
+                       :current-page="1"
+                       :page-sizes="[5, 10, 15]"
+                       :page-size="5"
+                       layout="total, sizes, prev, pager, next, jumper"
+                       :total="airTicketList.total">
+        </el-pagination>
       </div>
 
       <!-- 侧边栏 -->
@@ -31,25 +41,32 @@ import FlightsItem from '@/components/air/flightsItem'
 export default {
   data() {
     return {
-        airTicketList : []
+      airTicketList: []
     }
   },
   components: {
     FlightsListHead,
     FlightsItem
   },
-  mounted () {
-     
-      this.$axios({
-          url : 'airs',
-          params : this.$route.query
-      }).then((res)=>{
-          this.airTicketList = res.data
-          console.log(this.airTicketList);
-
-          
-      })
-      
+  mounted() {
+    this.$axios({
+      url: 'airs',
+      params: this.$route.query
+    }).then(res => {
+      this.airTicketList = res.data
+      console.log(this.airTicketList)
+    })
+  },
+  methods: {
+    //改变页码大小时触发
+    handleSizeChange(value) {
+      console.log('改变页码大小'+value)
+    },
+    //当前页发生改变时触发
+    handleCurrentChange(index){
+        console.log('页面改变'+index);
+        
+    }
   }
 }
 </script>
