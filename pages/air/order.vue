ca2755cd9dd32735bcf2a65d3bdbe37e40a76ed0
一打砖头拍不倒 <lyq15360562781@163.com>
2020-02-16 15:31:48 +0800

    订单侧边栏组件静态渲染完成

--- pages/air/order.vue
+++ pages/air/order.vue
@@ -1,38 +1,41 @@
 <template>
-    <div class="container">
-        <el-row type="flex" justify="space-between">
-            <!-- 订单表单 -->
-            <div class="main">
-                <OrderForm></OrderForm>
-            </div>
+  <div class="container">
+    <el-row type="flex"
+            justify="space-between">
+      <!-- 订单表单 -->
+      <div class="main">
+        <OrderForm></OrderForm>
+      </div>
 
-            <!-- 侧边栏 -->
-            <div class="aside">
-                          
-            </div>
-        </el-row>
-    </div>
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <OrderAside></OrderAside>
+      </div>
+    </el-row>
+  </div>
 </template>
 
 <script>
 import OrderForm from '@/components/air/orderForm'
+import OrderAside from '@/components/air/orderAside'
 export default {
-    components: {
-        OrderForm
-    }
+  components: {
+    OrderForm,
+    OrderAside
+  }
 }
 </script>
 
 <style lang="less" scoped>
-    .container{
-        width:1000px;
-        margin:20px auto;
-    }
-    
-    /*aside*/
-    .aside{
-        width: 350px;
-        height: fit-content;
-        border:1px #ddd solid;
-    }
+.container {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+/*aside*/
+.aside {
+  width: 350px;
+  height: fit-content;
+  border: 1px #ddd solid;
+}
 </style>
